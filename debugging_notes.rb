7beaf022a303d0b750cad0eec45bc7b1e129fe3d
BootCoder <bootcoder@gmail.com>
2015-03-19 09:26:57 -0700

    r1 slides & notes

--- debugging_notes.rb
+++ debugging_notes.rb
@@ -1,3 +1,7 @@
+require 'awesome_print'
+require 'pp'
+require 'pry-byebug'
+system 'clear'
 
 def raise_and_rescue
   begin
@@ -13,10 +17,18 @@ end
 raise_and_rescue
 
 
+
+
+
+
+
+
+
 class Name
 
   attr_reader :first, :last
   # When trying to set a first name, enforce rules about it.
+  binding.pry
   def first=(first)
     if first == nil or first.size == 0
       raise ArgumentError.new('Everyone must have a first name.')
@@ -27,3 +39,63 @@ class Name
   end
 
 end
+
+name_container = []
+
+10.times { name_container << Name.new }
+
+p "+" * 80
+p name_container
+p "+" * 80
+pp name_container
+p "+" * 80
+ap name_container
+p "+" * 80
+
+
+
+
+
+
+words_to_sample = %w[JibJab Dodad Whodhingy Thingermajig Flipflops FLapjacks bug Debug important Ruby Stuff to remember]
+some_array = Array.new(10) { Array.new(10) {words_to_sample.sample} }
+p "*" * 80
+p some_array
+p "*" * 80
+pp some_array
+p "*" * 80
+ap some_array
+p "*" * 80
+
+
+
+
+
+
+
+
+some_hash = {}
+
+words_to_sample.each_with_index { |item, idx| some_hash[item.to_sym] = idx}
+
+p "~" * 80
+p some_hash
+p "~" * 80
+pp some_hash
+p "~" * 80
+ap some_hash
+p "~" * 80
+
+
+
+
+
+
+
+
+
+
+
+
+
+
