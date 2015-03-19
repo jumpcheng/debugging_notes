a809a0dccccc88dcdd056b9b1f70f8fa74f01fa2
BootCoder <bootcoder@gmail.com>
2015-03-19 00:45:17 -0700

    update notes - add pry to slides

--- debugging_notes.rb
+++ debugging_notes.rb
@@ -2,7 +2,7 @@
 def raise_and_rescue
   begin
     puts 'I am before the raise.'
-    raise 'An error has occured.'
+    raise 'A super not awesome catastroflucked error has occurred.'
     puts 'I am after the raise.'
   rescue
     puts 'I am rescued.'
@@ -11,3 +11,19 @@ def raise_and_rescue
 end
 
 raise_and_rescue
+
+
+class Name
+
+  attr_reader :first, :last
+  # When trying to set a first name, enforce rules about it.
+  def first=(first)
+    if first == nil or first.size == 0
+      raise ArgumentError.new('Everyone must have a first name.')
+    end
+    first = first.dup
+    first[0] = first[0].chr.capitalize
+    @first = first
+  end
+
+end
