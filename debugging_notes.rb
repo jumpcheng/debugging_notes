d2cd80fed5aa4aa58965026a2f9d4b01e103c589
BootCoder <bootcoder@gmail.com>
2015-04-09 09:33:37 -0700

    refactor notess & slides

--- debugging_notes.rb
+++ debugging_notes.rb
@@ -1,7 +1,11 @@
+
+
 require 'awesome_print'
 require 'pp'
 require 'pry-byebug'
-system 'clear'
+
+
+# system 'clear'
 
 def raise_and_rescue
   begin
@@ -15,7 +19,7 @@ def raise_and_rescue
   puts 'I am after the begin block.'
 end
 
-raise_and_rescue
+# raise_and_rescue
 
 
 
@@ -27,37 +31,41 @@ raise_and_rescue
 
 class Name
 
-  # attr_reader :first, :last
-  # When trying to set a first name, enforce rules about it.
-  binding.pry
-  def first
-    p "b4 if"
-    if first == nil or first.size == 0
-      p "in the if"
-      raise ArgumentError.new('Everyone must have a first name.')
+  attr_accessor :name, :counter
+
+  def initialize
+    @name = ""
+    @counter = 0
+  end
+
+  # binding.pry
+  def count_me
+    until @counter == 5
+      # binding.pry
+      puts "Feed ME!!"
+      @counter += 1
     end
-    first = first.dup
-    first[0] = first[0].chr.capitalize
-    @first = first
   end
 
 end
 
 fred = Name.new
+# jan = Name.new(name: "jan")
 
-# fred.first(1,2,3)
+
+fred.count_me
 
 name_container = []
 
 10.times { name_container << Name.new }
 
-p "^" * 80
-p name_container
-p "=" * 80
-pp name_container
-p "!" * 80
-ap name_container
-p "+" * 80
+# p "^" * 80
+# p name_container
+# p "=" * 80
+# pp name_container
+# p "!" * 80
+# ap name_container
+# p "+" * 80
 
 
 
@@ -66,13 +74,14 @@ p "+" * 80
 
 words_to_sample = %w[JibJab Dodad Whodhingy Thingermajig Flipflops FLapjacks bug Debug important Ruby Stuff to remember]
 some_array = Array.new(10) { Array.new(10) {words_to_sample.sample} }
-p "*" * 80
-p some_array
-p "*" * 80
-pp some_array
-p "*" * 80
-ap some_array
-p "*" * 80
+
+# p "*" * 80
+# p some_array
+# p "*" * 80
+# pp some_array
+# p "*" * 80
+# ap some_array
+# p "*" * 80
 
 
 
@@ -85,13 +94,13 @@ some_hash = {}
 
 words_to_sample.each_with_index { |item, idx| some_hash[item.to_sym] = idx}
 
-p "~" * 80
-p some_hash
-p "~" * 80
-pp some_hash
-p "~" * 80
-ap some_hash
-p "~" * 80
+# p "~" * 80
+# p some_hash
+# p "~" * 80
+# pp some_hash
+# p "~" * 80
+# ap some_hash
+# p "~" * 80
 
 
 
